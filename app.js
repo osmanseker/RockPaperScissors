025e766bd7f44650685d498bd394020a2b793249
Osman Seker <50238194+osmanseker@users.noreply.github.com>
2022-07-14 16:05:14 +0200

    Update app.js
    problem with innerHTML, trying to solve that. it is supposed to show results in the console but it gives an error

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
-const playersocre = 0;
-const aisocre = 0;
+let playersocre = 0;
+let aisocre = 0;
 const playersocre_span = document.getElementById("player_score");
 const aisocre_span = document.getElementById("ai_score");
 const scoreboard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-function Game(playerChoice){
-    console.log(" choice is " + playerChoice);
+function getAIChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(playerChoice, aiChoice) {
+    playersocre++;
+    playersocre_span.innerHTML = playersocre;
+    aisocre_span.innerHTML = aisocre;
+    result_p.innerHTML = convertToWord(playerChoice) + "beats" + convertToWord(aiChoice) + "You win!";
+}
+
+function lose() {
+    aisocre++;
+}
+
+function draw() {
+
+}
+
+function Game(playerChoice) {
+    const aiChoice = getAIChoice();
+    switch (playerChoice + aiChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(playerChoice, aiChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(playerChoice, aiChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(playerChoice, aiChoice);
+            break;
+    }
 }
 
 function main() {
